b76adf38b238f1afb593030397ab87a2f309966d
est3banst <estebeeen@outlook.com>
2025-03-12 00:31:51 -0300

    me olvide

--- src/componentes/Form.jsx
+++ src/componentes/Form.jsx
@@ -50,7 +50,7 @@ const Form = () => {
         className='h-screen w-full flex inset-0 fixed top-0 z-[99232] justify-center left-0 bg-[#020202cd]'>
         <div
         onClick={(e) => e.stopPropagation()}
-        className='relative z-[-1] w-3/6 h-2/3 my-auto text-slate-50'>
+        className='relative z-[-1] p-4 w-5/6 md:w-3/6 h-2/3 my-auto text-slate-50'>
             <section className='text-xl p-6 flex flex-col relative z-50 bg-[#24242442] backdrop-blur-sm rounded-2xl gap-4 my-4'>
             <span 
         onClick={handleBtnModal}
@@ -61,7 +61,7 @@ const Form = () => {
                 </svg>
                </span>
                 <h2 className='font-black'>Esta aplicación es una demostración, no se enviará ningun dato</h2>
-                <p className='font-semibold'>Necesitas un tipo de solución como esta para tu negocio?</p>
+                <p className='font-semibold'>Estás en busqueda de una solución como esta para tu negocio ?</p>
                 <p className='font-semibold'>Podemos construirla acorde a tu negocio y sus necesidades especificas</p>
                 <a className='border bg-[#f2f2f248] hover:scale-105 transition-all duration-150 ease-out border-slate-600 p-3 mx-2 text-center'
                  href='https://kustomdev.com' 
